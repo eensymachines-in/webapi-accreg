712616887fba9889affdb52c984328d9933bc3cc
Niranjan <kneerunjun@gmail.com>
2022-11-11 05:45:37 +0530

    insert test done and #6

--- testdata.go
+++ testdata.go
@@ -1,17 +1,18 @@
 package main
 
+/* ==============================================
+Copyright (c) Eensymachines
+Developed by 		: kneerunjun@gmail.com
+Developed on 		: OCT'22
+This was developed to read and write test data and make it available as in memory slice
+test data resides in json alongside
+============================================== */
 import (
 	"encoding/json"
 	"io/ioutil"
 	"math/rand"
 )
 
-/* ==============================================
-Copyright (c) Eensymachines
-Developed by 		: kneerunjun@gmail.com
-Developed on 		: OCT'22
-This was developed to read and write test data and make it available as an in mem slice of data
-============================================== */
 // JsonSampleUserAccounts : shall read the entire file and then send back the block result
 func JsonSampleUserAccounts() ([]*UserAccount, error) {
 	byt, err := ioutil.ReadFile("useraccs.json")
